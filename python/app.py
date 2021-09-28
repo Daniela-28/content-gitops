f794b721e87123f2a0216ebb142c71f88b8a9b25
Daniela-28 <91548775+Daniela-28@users.noreply.github.com>
2021-09-28 11:02:17 -0500

    Update 4

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello CDMX Students!"
+    return "Hello Mexico City Students!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
